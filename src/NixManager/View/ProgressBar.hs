c1d4ffdb16917d42e54d176765b2a21c4707305d
Philipp Middendorf <middendorf@plapadoo.de>
2020-03-27 11:30:23 +0100

    ♻ Simplify progressBar using a custom widget

--- src/NixManager/View/ProgressBar.hs
+++ src/NixManager/View/ProgressBar.hs
@@ -1,99 +1,50 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE OverloadedLabels #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedLists #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module NixManager.View.ProgressBar
   ( progressBar
   )
 where
 
-import           Control.Monad                  ( when )
+import           GI.Gtk.Declarative.Attributes.Internal
+                                                ( addSignalHandler )
 import           Data.Vector                    ( Vector )
-import           GI.Gtk.Declarative             ( Attribute )
+import           GI.Gtk.Declarative             ( Widget(Widget)
+                                                , CustomWidget(CustomWidget)
+                                                , customWidget
+                                                , customCreate
+                                                , Attribute
+                                                , customPatch
+                                                , customSubscribe
+                                                , customAttributes
+                                                , customParams
+                                                , CustomPatch
+                                                  ( CustomKeep
+                                                  , CustomModify
+                                                  )
+                                                )
 import qualified GI.Gtk                        as Gtk
-import           Data.Typeable                  ( eqT
-                                                , (:~:)(Refl)
-                                                )
-import           GI.Gtk.Declarative.Patch       ( Patchable(create, patch)
-                                                , Patch(Replace, Modify)
-                                                )
-import           GI.Gtk.Declarative.State       ( SomeState(SomeState)
-                                                , stateTreeCustomState
-                                                , stateTreeCollectedAttributes
-                                                , StateTree(StateTreeWidget)
-                                                , StateTreeNode(StateTreeNode)
-                                                , stateTreeNode
-                                                , stateTreeNodeWidget
-                                                , stateTreeStyleContext
-                                                )
-import           GI.Gtk.Declarative.EventSource ( EventSource
-                                                , subscribe
-                                                , fromCancellation
-                                                )
-import           GI.Gtk.Declarative.Attributes.Collected
-                                                ( collectAttributes
-                                                , collectedClasses
-                                                , collectedProperties
-                                                , constructProperties
-                                                , updateProperties
-                                                , canBeModifiedTo
-                                                , updateClasses
-                                                )
-import           GI.Gtk.Declarative.Widget      ( Widget(Widget) )
 
-data ProgressBar e = ProgressBar (Vector (Attribute Gtk.ProgressBar e)) Int
-  deriving(Functor)
-
-instance EventSource ProgressBar where
-  subscribe _ _ _ = pure (fromCancellation (pure ()))
-
-instance Patchable ProgressBar where
-  create (ProgressBar props _) = do
-    let collected = collectAttributes props
-    widget <- Gtk.new Gtk.ProgressBar (constructProperties collected)
-    Gtk.progressBarPulse widget
-    Gtk.widgetShow widget
-    updateProperties widget mempty (collectedProperties collected)
-    styleContext <- Gtk.widgetGetStyleContext widget
-    pure
-      (SomeState
-        (StateTreeWidget (StateTreeNode widget styleContext collected ()))
-      )
-  patch (SomeState (stateTree :: StateTree st w e c cs)) (ProgressBar _ cold) new@(ProgressBar newAttrs cnew)
-    = case eqT @w @Gtk.ProgressBar of
-      Just Refl ->
-        let
-          oldCollected = stateTreeCollectedAttributes (stateTreeNode stateTree)
-          newCollected = collectAttributes newAttrs
-          oldCollectedProps = collectedProperties oldCollected
-          newCollectedProps = collectedProperties newCollected
-          canBeModified = oldCollectedProps `canBeModifiedTo` newCollectedProps
-        in
-          if not canBeModified
-            then Replace (create new)
-            else Modify $ do
-              let widget' = stateTreeNodeWidget stateTree
-              updateProperties widget' oldCollectedProps newCollectedProps
-              updateClasses (stateTreeStyleContext (stateTreeNode stateTree))
-                            (collectedClasses oldCollected)
-                            (collectedClasses newCollected)
-              let node = stateTreeNode stateTree
-              casted <- Gtk.unsafeCastTo Gtk.ProgressBar widget'
-              when (cold /= cnew) (Gtk.progressBarPulse casted)
-              pure
-                (SomeState
-                  (StateTreeWidget node
-                    { stateTreeCustomState         = ()
-                    , stateTreeCollectedAttributes = newCollected
-                    }
-                  )
-                )
-      Nothing -> Replace (create new)
 
 progressBar :: Vector (Attribute Gtk.ProgressBar e) -> Int -> Widget e
-progressBar attrs counter = Widget (ProgressBar attrs counter)
+progressBar customAttributes customParams = Widget
+  (CustomWidget { customWidget
+                , customCreate
+                , customPatch
+                , customSubscribe
+                , customAttributes
+                , customParams
+                }
+  )
+ where
+  customWidget = Gtk.ProgressBar
+  customCreate :: Int -> IO (Gtk.ProgressBar, ())
+  customCreate _ = do
+    widget <- Gtk.new Gtk.ProgressBar []
+    Gtk.progressBarPulse widget
+    pure (widget, ())
+  customSubscribe _params _internalState widget cb =
+    foldMap (addSignalHandler cb widget) customAttributes
+  customPatch before after _internalState
+    | before == after = CustomKeep
+    | otherwise = CustomModify $ \widget -> do
+      Gtk.progressBarPulse widget
+      pure ()
